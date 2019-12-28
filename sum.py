a5809a3e542d3ee1a0d66cfee80061a0c39ce7f2
udaykumarsamala <58910376+udaykumarsamala@users.noreply.github.com>
2019-12-28 14:54:57 +0530

    Update sum.py

--- sum.py
+++ sum.py
@@ -1,6 +1,10 @@
-a = int(input("enter first number: "))
-b = int(input("enter second number: "))
- 
-sum = a + b
- 
-print("sum:", sum)
+# Python3 program to add two numbers 
+
+num1 = 15
+num2 = 12
+
+# Adding two nos 
+sum = num1 + num2 
+
+# printing values 
+print("Sum of {0} and {1} is {2}" .format(num1, num2, sum)) 
